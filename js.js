3ffc0a3670f555536f00ae7ea03e9cab96765c99
GoddessSeshat <196391177+GoddessSeshat@users.noreply.github.com>
2025-03-26 05:44:11 +0200

    completed ComputerChoices

--- js.js
+++ js.js
@@ -1,1 +1,17 @@
-console.log("Hello World")
+function getComputerChoice() {
+
+    choices = Math.floor(Math.random() * 3)
+
+    if (choices == 0) {
+        alert("rock");
+    } else if (choices == 1) {
+        alert("paper");
+    } else if (choices == 2) {
+        alert ("scissors")
+    }
+
+return choices;
+
+}
+
+console.log(getComputerChoice());
