044c4c269df9ea569d649e6a5f678bf6d1cf11cd
GoddessSeshat <196391177+GoddessSeshat@users.noreply.github.com>
2025-03-26 06:23:31 +0200

    edited ComputerChoice by replacing alert with console.log for clarity and completed HumanChoice.

--- js.js
+++ js.js
@@ -3,11 +3,11 @@ function getComputerChoice() {
     choices = Math.floor(Math.random() * 3)
 
     if (choices == 0) {
-        alert("rock");
+        console.log("Computer chose rock");
     } else if (choices == 1) {
-        alert("paper");
+        console.log("Computer chose paper");
     } else if (choices == 2) {
-        alert ("scissors")
+        console.log("Computer chose scissors");
     }
 
 return choices;
@@ -15,3 +15,27 @@ return choices;
 }
 
 console.log(getComputerChoice());
+
+
+
+
+
+function getHumanChoice() {
+    let choice1 = "rock";
+    let choice2 = "paper";
+    let choice3 = "scissors";
+
+    let playerChoice = prompt("Your turn:", ["rock, paper, scissors"]);
+
+    if (playerChoice == choice1) {
+        console.log("HumanPlayer chose rock");
+    } else if (playerChoice == choice2) {
+        console.log("HumanPlayer chose paper");
+    } else if (playerChoice == choice3) {
+        console.log("HumanPlayer chose scissors");
+    }
+    
+    return playerChoice;
+}
+
+console.log(getHumanChoice());
